facf2d6e3a4760a2b97cb9ed885bc84783dd95c3
gonza1595 <aveirogonzalo3cnp@gmail.com>
2023-11-09 15:05:02 -0300

    product/services

---
+++ src/services/ProductServices.ts
@@ -0,0 +1,53 @@
+import { Product } from "../types/Product";
+
+const BASE_URL = "api/v1/articulos";
+
+export const ProductServices = {
+
+    getProducts: async (): Promise<Product[]> => {
+        const response = await fetch(`${BASE_URL}/manufacturados`);
+        const data = await response.json();
+    
+        return data;
+    },
+
+    getProduct: async (id: number): Promise<Product> => {
+    const response = await fetch(`${BASE_URL}/manufacturados/${id}`);
+    const data = await response.json();
+        
+        return data;    
+    },
+
+    createProduct:async (product:Product): Promise<Product> => {
+        const response = await fetch(`${BASE_URL}/manufacturados`, {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(product)
+        }); 
+        const data = await response.json();
+
+        return data;
+    },
+
+    updateProduct:async (id:number,product:Product): Promise<Product> => {
+        const response = await fetch(`${BASE_URL}/manufacturados/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(product)
+        }); 
+        const data = await response.json();
+
+        return data;
+    },
+
+    deleteProduct: async (id: number): Promise<void> => {
+        await fetch(`${BASE_URL}/manufacturados/${id}`, {
+            method: "DELETE"
+        });
+    }
+ 
+}
